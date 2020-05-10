07ad1f11d9688d8c27bed9ba02c50d7d2be16d7c
Shlok Somani <shloksomani@ymail.com>
2020-05-09 20:01:38 -0400

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -164,7 +164,6 @@ class Contact extends React.Component {
             <div className="row">
               <div className="col-sm-12">
                 <div className="copyright-box">
-                  {/* <p className="copyright">2019 &copy; Copyright <strong>Frank Izquierdo</strong>. All Rights Reserved</p> */}
                 </div>
               </div>
             </div>
