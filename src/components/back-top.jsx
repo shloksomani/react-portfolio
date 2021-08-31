6bbe53521aeaf723a56ce60bd86193f6f446f401
Joseph Panetta <jlpanetta1681@icloud.com>
2021-08-31 18:24:02 -0400

    changed the script pointer sin package.json

--- src/components/back-top.jsx
+++ src/components/back-top.jsx
@@ -21,7 +21,7 @@ class BackToTop extends React.Component {
     }
 
     render(){
-        return <a href="#" className="back-to-top animated"><i className="fa fa-chevron-up"></i></a>;
+        return <a href="/" className="back-to-top animated"><i className="fa fa-chevron-up"></i></a>;
     }
 }
 
